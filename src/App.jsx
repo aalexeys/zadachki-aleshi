1bd3747defdc4e2a3dfd489c96915952a5ab4f88
afmikhail <pushkavpn@gmail.com>
2026-04-27 01:13:15 +0300

    Update login screen text

--- src/App.jsx
+++ src/App.jsx
@@ -841,7 +841,7 @@ export default function ProjectTodoDashboard() {
           </button>
 
           <div className="mt-5 rounded-2xl bg-slate-50 p-4 text-xs leading-5 text-slate-500">
-            Данные аккаунтов хранятся в Firebase Firestore и синхронизируются между устройствами.
+            Вот это нихуя себе даже вход через гугл есть, еще и firebase оу ес
           </div>
         </div>
       </div>
